0aac295a6ecbda362a5935cd81fd3ad4992d1822
ActualMecha <fordaguk@begoz.com>
2016-12-03 02:32:25 +0300

    spawning one creep for each mining spot

--- main.js
+++ main.js
@@ -4,34 +4,64 @@ let debug = require("utils.debug");
 let global = require("utils.globals");
 debug.turnOn();
 
-function processCreeps() {
-    var totalCreeps = 0;
-    for (var creepName in Game.creeps) {
-        var creep = Game.creeps[creepName];
-        
+if (Memory.rooms === undefined) {
+    Memory.rooms = {};
+}
+
+function processCreeps(room) {
+    var totalHarvesters = 0;
+    room.find(FIND_MY_CREEPS).forEach(function (creep) {
         if (creep.spawning) {
-            continue;
+            return;
         }
-        
+    
         switch (creep.memory.role) {
         case global.Role.HARVESTER: 
-            ++totalCreeps;
+            ++totalHarvesters;
             roleHarvester.act(creep); 
             break;
         }
-    }
-    Memory.creepsCount = totalCreeps;
+    });
+    var creeps = [];
+    creeps[global.Role.HARVESTER] = totalHarvesters;
+    Memory.rooms[room.name].creeps = creeps;
 }
 
-function processSpawns() {
-    for (var spawnName in Game.spawns) {
-        var spawn = Game.spawns[spawnName];
+function processSpawns(room) {
+    room.find(FIND_MY_SPAWNS).forEach(function (spawn) {
         roleSpawner.run(spawn);
+    });
+}
+
+function processRoom(room) {
+    if (Memory.rooms[room.name] === undefined) {
+        var miningSpots = [];
+        room.find(FIND_SOURCES).forEach(function (source) {
+            var position = source.pos;
+            for (var x = position.x - 1; x <= position.x + 1; ++x) {
+                for (var y = position.y - 1; y <= position.y + 1; ++y) {
+                    var passable = true;
+                    var spot = new RoomPosition(x, y, room.name);
+                    spot.look(LOOK_TERRAIN).forEach(function (object) {
+                        passable &= OBSTACLE_OBJECT_TYPES.indexOf(object.terrain) == -1;
+                    });
+                    if (passable) {
+                        miningSpots.push({spot, occupied: false});
+                    }
+                }
+            }
+        });
+        Memory.rooms[room.name] = {
+            miningSpots: miningSpots
+        };
     }
 }
 
 module.exports.loop = function() {
-    debug.log("looping");
-    processCreeps();
-    processSpawns();
+    for (var roomName in Game.rooms) {
+        var room = Game.rooms[roomName];
+        processRoom(room);
+        processCreeps(room);
+        processSpawns(room);
+    }
 }
--- role.spawner.js
+++ role.spawner.js
@@ -2,8 +2,8 @@ let global = require("utils.globals");
 
 module.exports = {
     run: function(spawn) {
-        if (Memory.creepsCount < 10 && !spawn.spawning && spawn.energy >= 200)  {
-            
+        var miningSpots = Memory.rooms[spawn.room.name].miningSpots;
+        if (Memory.creepsCount < miningSpots.length && !spawn.spawning && spawn.energy >= 200)  {
             spawn.createCreep([WORK, CARRY, MOVE], {role: global.Role.HARVESTER});
         }
     }
