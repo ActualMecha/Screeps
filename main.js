80aaf717ae1d32a76c49a1aa4e42fbb0115e5fe2
ActualMecha <fordaguk@begoz.com>
2016-12-03 00:01:35 +0300

    moved one lvl down

---
+++ main.js
@@ -0,0 +1,37 @@
+let roleHarvester = require("role.harvester");
+let roleSpawner = require("role.spawner");
+let debug = require("utils.debug");
+let global = require("utils.globals");
+debug.turnOn();
+
+function processCreeps() {
+    var totalCreeps = 0;
+    for (var creepName in Game.creeps) {
+        var creep = Game.creeps[creepName];
+        
+        if (creep.spawning) {
+            continue;
+        }
+        
+        switch (creep.memory.role) {
+        case global.Role.HARVESTER: 
+            ++totalCreeps;
+            roleHarvester.act(creep); 
+            break;
+        }
+    }
+    Memory.creepsCount = totalCreeps;
+}
+
+function processSpawns() {
+    for (var spawnName in Game.spawns) {
+        var spawn = Game.spawns[spawnName];
+        roleSpawner.run(spawn);
+    }
+}
+
+module.exports.loop = function() {
+    debug.log("looping");
+    processCreeps();
+    processSpawns();
+}
