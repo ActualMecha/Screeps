55bb2f09808319e98efb511ebba105be556a1461
ActualMecha <fordaguk@begoz.com>
2016-12-03 13:25:15 +0300

    no stucking when an object is built

--- role.harvester.js
+++ role.harvester.js
@@ -1,6 +1,20 @@
 let debug = require("utils.debug")
 let resourceManager = require("manager.resources");
 
+module.exports = {
+    act: function(creep) {
+        if (!creep.memory.initialized) {
+            initialize(creep);
+        }
+        switch (creep.memory.state) {
+        case State.GoingToSource: gotoSource(creep); break;
+        case State.Mining: mine(creep); break;
+        case State.GoingToTarget: gotoTarget(creep); break;
+        case State.Transferring: transfer(creep); break;
+        }   
+    }
+};
+
 let State = {
     GoingToSource: 2,
     Mining: 3,
@@ -43,51 +57,57 @@ function planRoad(creep) {
     }
 }
 
-module.exports = {
-    act: function(creep) {
-        if (!creep.memory.initialized) {
-            initialize(creep);
-        }
-        var miningSpot = creep.memory.miningSpot;
-        switch (creep.memory.state) {
-        case State.GoingToSource:
-            creep.moveTo(miningSpot.x, miningSpot.y);
-            planRoad(creep);
-            if (creep.pos.x == miningSpot.x && creep.pos.y == miningSpot.y) {
-                creep.memory.state = State.Mining;
-            }
-            break;
-            
-        case State.Mining:
-            creep.harvest(Game.getObjectById(miningSpot.sourceId));
-            if (creep.carryCapacity == creep.carry.energy) {
-                creep.memory.state = State.GoingToTarget;
-            }
-            break;
-            
-        case State.GoingToTarget:
-            if (!creep.memory.target) {
-                let sink = resourceManager.findEnergySink(creep);
-                if (sink) {
-                    creep.memory.target = sink.id;
-                }
-            }
-            var target = Game.getObjectById(creep.memory.target);
-            creep.moveTo(target);
-            if (creep.pos.isNearTo(target)) {
-                creep.memory.state = State.Transferring;
-            }
-            break;
-            
-        case State.Transferring:
-            var target = Game.getObjectById(creep.memory.target);
-            creep.memory.buildRoad = target.structureType == STRUCTURE_SPAWN || target.structureType == STRUCTURE_CONTROLLER;
-            creep.transfer(target, RESOURCE_ENERGY);
-            if (creep.carry.energy == 0) {
-                creep.memory.state = State.GoingToSource;
-                creep.memory.target = undefined;
-            }
-            break;
-        }   
+function gotoSource(creep) {
+    var miningSpot = creep.memory.miningSpot;
+    creep.moveTo(miningSpot.x, miningSpot.y);
+    planRoad(creep);
+    if (creep.pos.x == miningSpot.x && creep.pos.y == miningSpot.y) {
+        creep.memory.state = State.Mining;
     }
-};
+}
+
+function mine(creep) {
+    creep.harvest(Game.getObjectById(creep.memory.miningSpot.sourceId));
+    if (creep.carryCapacity == creep.carry.energy) {
+        creep.memory.state = State.GoingToTarget;
+    }
+}
+
+function gotoTarget(creep) {
+    if (!creep.memory.target) {
+        let sink = resourceManager.findEnergySink(creep);
+        if (sink) {
+            creep.memory.target = sink.id;
+        }
+    }
+    var target = Game.getObjectById(creep.memory.target);
+    creep.moveTo(target);
+    if (creep.pos.isNearTo(target)) {
+        creep.memory.state = State.Transferring;
+    }
+}
+
+function transfer(creep) {
+    var target = Game.getObjectById(creep.memory.target);
+    var buildMode = target instanceof ConstructionSite;
+    creep.memory.buildRoad = !buildMode;
+    
+    if (!creep.pos.isNearTo(target)) {
+        creep.memory.state = State.GoingToTarget;
+    }
+    else if (buildMode && target.progress < target.progressTotal) {
+        creep.build(target);
+    }
+    else if (!buildMode && target.energy < target.energyCapacity) {
+        creep.transfer(target, RESOURCE_ENERGY);
+    }
+    else {
+        creep.memory.state = State.GoingToTarget;
+        creep.memory.target = undefined;
+    }
+    
+    if (creep.carry.energy == 0) {
+        creep.memory.state = State.GoingToSource;
+        creep.memory.target = undefined;
+    }
+}
