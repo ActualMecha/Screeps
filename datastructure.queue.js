d86b77fb79d34a46a7104a87a42c9c35c1bb947a
ActualMecha <fordaguk@begoz.com>
2017-01-02 17:01:21 +0300

    Queue as a real class

--- datastructure.queue.js
+++ datastructure.queue.js
@@ -1,40 +1,45 @@
-module.exports = function Queue(memory) {
-    this.push = function(data) {
-        this._tail.push(data);
-    };
+class Queue {
+    constructor(memory) {
+        if (!memory) {
+            this._head = [];
+            this._tail = [];
+        }
+        else {
+            this._head = memory._head;
+            this._tail = memory._tail;
+        }
+    }
 
-    this.peek = function() {
+    push(data) {
+        this._tail.push(data);
+    }
+
+
+    peek() {
         this._repopulate();
         return this._head[this._head.length - 1];
-    };
+    }
 
-    this.pop = function() {
+    pop() {
         this._repopulate();
         return this._head.pop();
-    };
+    }
 
-    this.isEmpty = function() {
+    isEmpty() {
         return this.length() == 0;
-    };
+    }
 
-    this.length = function() {
+    length() {
         return this._head.length + this._tail.length;
-    };
+    }
 
-    this._repopulate = function() {
+    _repopulate() {
         if (this._head.length == 0) {
             while (this._tail.length > 0) {
                 this._head.push(this._tail.pop());
             }
         }
-    };
+    }
+}
 
-    if (!memory) {
-        this._head = [];
-        this._tail = [];
-    }
-    else {
-        this._head = memory._head;
-        this._tail = memory._tail;
-    }
-};
+module.exports = Queue;
